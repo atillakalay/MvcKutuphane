7485829e2b708e55fc1390803372ae2ea5b86b9d
atilla kalay <atilla.kalayy@gmail.com>
2021-01-27 12:48:02 +0300

    Add project files.

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+namespace MvcKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        private DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KategoriEkle(TBLKATEGORI p)
+        {
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = db.TBLKATEGORI.Find(id);
+            return View("KategoriGetir", kategori);
+        }
+
+        public ActionResult KategoriGuncelle(TBLKATEGORI p)
+        {
+            var kategori = db.TBLKATEGORI.Find(p.ID);
+            kategori.AD = p.AD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MvcKutuphane/Controllers/KitapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KitapController : Controller
+    {
+        // GET: Kitap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
