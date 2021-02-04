a38ab5e6170bf6432360ac68afc02c2807dab2ab
atilla kalay <atilla.kalayy@gmail.com>
2021-02-04 12:29:00 +0300

    Login işlemleri eklendi

--- MvcKutuphane/Controllers/LoginController.cs
+++ MvcKutuphane/Controllers/LoginController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        private DBKUTUPHANEEntities2 dbkutuphaneEntities2 = new DBKUTUPHANEEntities2();
         public ActionResult GirisYap()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult GirisYap(TBLUYELER p)
+        {
+            var bilgiler = dbkutuphaneEntities2.TBLUYELER.FirstOrDefault(x => x.MAIL == p.MAIL && x.SIFRE == p.SIFRE);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.MAIL, false);
+                Session["Mail"] = bilgiler.MAIL.ToString();
+                return RedirectToAction("Index", "Panelim");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- MvcKutuphane/Controllers/PanelimController.cs
+++ MvcKutuphane/Controllers/PanelimController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
 {
     public class PanelimController : Controller
     {
         // GET: Panelim
+        private DBKUTUPHANEEntities2 dbkutuphaneEntities2 = new DBKUTUPHANEEntities2();
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var uyemail = (string)Session["Mail"];
+            var degerler = dbkutuphaneEntities2.TBLUYELER.FirstOrDefault(z => z.MAIL == uyemail);
+            return View(degerler);
+        }
+        [HttpPost]
+        public ActionResult Index2(TBLUYELER p)
+        {
+            var kullanici = (string)Session["Mail"];
+            var uye = dbkutuphaneEntities2.TBLUYELER.FirstOrDefault(x => x.MAIL == kullanici);
+            uye.SIFRE = p.SIFRE;
+            uye.AD = p.AD;
+            uye.FOTOGRAF = p.FOTOGRAF;
+            uye.OKUL = p.OKUL;
+            uye.KULLANICIADI = p.KULLANICIADI;
+            dbkutuphaneEntities2.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
